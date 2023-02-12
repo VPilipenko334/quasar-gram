2f81a053ebcc5aad6935b816db5a812c8b36fccd
Veronika Pilipenko <veronika.pilipenko@gmail.com>
2023-02-11 21:10:32 -0500

    added images

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -32,7 +32,7 @@
       </q-tabs>
     </q-footer>
 
-    <q-page-container>
+    <q-page-container class="bg-grey-1">
       <router-view />
     </q-page-container>
   </q-layout>
--- quasar-project/src/pages/PageHome.vue
+++ quasar-project/src/pages/PageHome.vue
@@ -1,13 +1,74 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constain q-pa-md">
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="my-card q-mb-md"
+      flat
+      bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://avatars.githubusercontent.com/u/63820576?v=4" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">veronika_instagram</q-item-label>
+          <q-item-label caption> {{ post.location }} </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img :src="post.imageUrl" />
+
+      <q-card-section>
+        <div>{{ post.caption }}</div>
+        <div class="text-caption text-grey">{{ post.date }}</div>
+      </q-card-section>
+
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'PageHome'
-})
+export default {
+  name: "PageHome",
+    data() {
+      return {
+        posts: [
+          {
+            id: 1,
+            caption: 'My little Mishka.',
+            date: 'May 5th 11:11 A.M',
+            location: 'New York, New York',
+            imageUrl: 'images/mishka.jpeg'
+          },
+          {
+            id: 2,
+            caption: 'Mishkas tongue sticking out in the summertime.',
+            date: 'September 3rd, 2:45 P.M. ',
+            location: 'Lower East Side, New York',
+            imageUrl: 'images/puppy.JPG'
+          },
+          {
+            id: 3,
+            caption: 'Best friends in the snow.',
+            date: 'December 2nd 4:54 P.M. ',
+            location: 'PA, Pennsylvania',
+            imageUrl: 'images/best_friends.jpeg'
+          },
+          {
+            id: 4,
+            caption: 'Mishkass baby teeth coming in.',
+            date: 'June 27th, 9:43 A.M.',
+            location: 'East Village, New York',
+            imageUrl: 'images/baby_mish.jpeg'
+          },
+        ]
+      }
+    },
+};
 </script>
