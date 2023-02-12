afe8f827bba3947ca205c695daf7b9a7d22d855c
Veronika Pilipenko <veronika.pilipenko@gmail.com>
2023-02-11 21:50:15 -0500

    added more things to the home page

--- quasar-project/src/pages/PageCamera.vue
+++ quasar-project/src/pages/PageCamera.vue
@@ -1,13 +1,42 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="images/mishka.jpeg" />
+    </div>
+
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col" label="Caption" dense />
+      </div>
+
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col" label="Location" dense>
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+
+
+      <div class="row justify-center q-mt-lg">
+        <q-btn unelevated rounded color="primary" label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageCamera'
-})
+  name: "PageCamera",
+});
 </script>
+
+<style scoped>
+.camera-frame {
+  border: 2px solid grey;
+}
+</style>
--- quasar-project/src/pages/PageHome.vue
+++ quasar-project/src/pages/PageHome.vue
@@ -1,74 +1,93 @@
 <template>
   <q-page class="constain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="my-card q-mb-md"
-      flat
-      bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://avatars.githubusercontent.com/u/63820576?v=4" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="my-card q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img
+                  src="https://avatars.githubusercontent.com/u/63820576?v=4"
+                />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">veronika_instagram</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">veronika_instagram</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
+          <q-separator />
 
-      <q-img :src="post.imageUrl" />
+          <q-img :src="post.imageUrl" />
 
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ post.date }}</div>
-      </q-card-section>
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://avatars.githubusercontent.com/u/63820576?v=4" />
+            </q-avatar>
+          </q-item-section>
 
-    </q-card>
+          <q-item-section>
+            <q-item-label class="text-bold">veronika_instagram</q-item-label>
+            <q-item-label caption> Veronika Pilipenko </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-
 export default {
   name: "PageHome",
-    data() {
-      return {
-        posts: [
-          {
-            id: 1,
-            caption: 'My little Mishka.',
-            date: 'May 5th 11:11 A.M',
-            location: 'New York, New York',
-            imageUrl: 'images/mishka.jpeg'
-          },
-          {
-            id: 2,
-            caption: 'Mishkas tongue sticking out in the summertime.',
-            date: 'September 3rd, 2:45 P.M. ',
-            location: 'Lower East Side, New York',
-            imageUrl: 'images/puppy.JPG'
-          },
-          {
-            id: 3,
-            caption: 'Best friends in the snow.',
-            date: 'December 2nd 4:54 P.M. ',
-            location: 'PA, Pennsylvania',
-            imageUrl: 'images/best_friends.jpeg'
-          },
-          {
-            id: 4,
-            caption: 'Mishkass baby teeth coming in.',
-            date: 'June 27th, 9:43 A.M.',
-            location: 'East Village, New York',
-            imageUrl: 'images/baby_mish.jpeg'
-          },
-        ]
-      }
-    },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "My little Mishka.",
+          date: "May 5th 11:11 A.M",
+          location: "New York, New York",
+          imageUrl: "images/mishka.jpeg",
+        },
+        {
+          id: 2,
+          caption: "Mishkas tongue sticking out in the summertime.",
+          date: "September 3rd, 2:45 P.M. ",
+          location: "Lower East Side, New York",
+          imageUrl: "images/puppy.JPG",
+        },
+        {
+          id: 3,
+          caption: "Best friends in the snow.",
+          date: "December 2nd 4:54 P.M. ",
+          location: "PA, Pennsylvania",
+          imageUrl: "images/best_friends.jpeg",
+        },
+        {
+          id: 4,
+          caption: "Mishkass baby teeth coming in.",
+          date: "June 27th, 9:43 A.M.",
+          location: "East Village, New York",
+          imageUrl: "images/baby_mish.jpeg",
+        },
+      ],
+    };
+  },
 };
 </script>
